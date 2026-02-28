0aab4606bddf5a4bd98aa5fee72a63835ff99fd5
ambagwa <ambagwaeugene09@gmail.com>
2026-02-28 15:06:18 +0300

    Refactor Navbar

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -4,7 +4,7 @@ const NavLink = ({ children }) => {
   return (
     <a
       href="#"
-      className="px-2 hover:text-orange relative inline-block
+      className="px-2 py-3 sm:py-0 hover:text-orange relative block sm:inline-block
             after:content-[''] after:absolute after:left-1/2 after:-translate-x-1/2 after:bottom-0
             after:h-[2px] after:w-0 after:bg-orange
             after:transition-all after:duration-300
@@ -31,7 +31,7 @@ export const Navbar = () => {
 
   return (
     <>
-      <nav className="max-w-screen bg-neutral-100 hover:bg-neutral-300 p-4 mt-2 mx-4 lg:mx-40 rounded-sm flex items-center justify-between shadow-lg">
+      <nav className="max-w-screen bg-neutral-100 hover:bg-neutral-300 p-4 mt-2 mx-4 lg:mx-40 rounded-sm flex flex-col sm:flex-row items-start sm:items-center justify-between shadow-lg">
         {/*logo container */}
         <div className="flex items-center gap-2">
           <img
@@ -44,15 +44,57 @@ export const Navbar = () => {
           </span>
         </div>
 
+        {/*Desktop view */}
         {/*Navigation links */}
-        <div className="flex items-center gap-2 font-bold">
+        <div className="items-center gap-2 font-bold hidden sm:flex">
           {navIcons.map((item) => (
             <NavLink key={item}>{item}</NavLink>
           ))}
         </div>
 
         {/*Icons */}
-        <div className="flex items-center gap-4 font-bold">
+        <div className="items-center gap-4 font-bold hidden sm:flex">
+          <NavIcon>
+            <svg
+              fill="none"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className={`${hoverIcon}`}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+              />
+            </svg>
+          </NavIcon>
+
+          <NavIcon>
+            <svg
+              fill="none"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className={`${hoverIcon}`}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+              />
+            </svg>
+          </NavIcon>
+        </div>
+
+        {/** Mobile view */}
+        {/*Navigation links */}
+        <div className="items-center gap-2 font-bold sm:hidden mt-4 sm:mt-0">
+          {navIcons.map((item) => (
+            <NavLink key={item}>{item}</NavLink>
+          ))}
+        </div>
+
+        {/*Icons */}
+        <div className="items-center gap-4 font-bold sm:hidden flex mt-4 sm:mt-0">
           <NavIcon>
             <svg
               fill="none"
