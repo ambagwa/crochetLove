460ae6fe9d1c10e67c62275c028b2a49e244045a
ambagwa <ambagwaeugene09@gmail.com>
2026-03-04 21:30:44 +0300

    Add CustomOrder section

---
+++ client/src/components/sections/custom_order/CustomOrder.jsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const CustomOrder = () => {
+  return (
+    <div className="mt-5 p-12 bg-orange-300">
+      <h1 className="text-center font-bold text-3xl sm:text-5xl">
+        Looking to curate something unique? We got you
+      </h1>
+      <Button
+        variant="outline"
+        size="lg"
+        className="flex mt-16 text-lg mx-auto hover:cursor-pointer hover:bg-orange-500 hover:text-white"
+      >
+        Request a custom item
+      </Button>
+    </div>
+  );
+};
+
+export default CustomOrder;
