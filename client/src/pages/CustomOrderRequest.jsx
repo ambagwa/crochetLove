87552e3cd8d37c846618b90c1b10c322c98c6e5b
ambagwa <ambagwaeugene09@gmail.com>
2026-03-10 20:22:15 +0300

    Add CustomOrderRequest

--- client/src/pages/CustomOrderRequest.jsx
+++ client/src/pages/CustomOrderRequest.jsx
@@ -1,6 +1,65 @@
-export const CustomOrderRequest = () => {
+export const CustomOrderRequest = () = {
+	const process = [
+	  "Submit your request with details",
+	  "A quotation will be sent within 24 hrs",
+	  "Approve the quote and make a 70% deposit",
+	  "Create a custom piece",
+	  "Pay the balance and have your order shipped"
+	];
+
   return (
-    <div className="grid grid-cols-5"></div>
+	  <div className="m-10">
+	    <p className="py-5 font-medium text-3xl sm:text-5xl">
+		Custom Order Request
+	  </p>
+
+    <div className="grid grid-cols-1 sm:grid-cols-5 py-10">
+	  {/*Left section*/}
+      <div className="col-span-3">
+        	<p className="text-2xl">
+		  Do o=you have a unique idea you wanna curate? Share them
+	  	  with us and we will create a custom piece just for 
+	  	  <span className="text-orange underline">YOU</span>
+	  	  Provide as many details as possible to help bring your idea
+	  	  into life
+	  </p>
+
+	  {/* Form*/}
+	  <form>
+	    <label htmlFor="name">Your name:</label>
+	    <input type="text" id="name" />
+	    <label htmlFor="contact">Phone Number:</label>                                                                                                                                                                     <input type="number" id="name" /> 
+	    <label htmlFor="email">Email Address</label>
+	    <input type="email" id="email" />
+	    <label htmlFor="description">Describe your custom order</label>
+	    <textarea id="description" rows="10" placeholder="Include details like: size, color, purpose, any special requests"></textarea> 
+	    <label htmlFor="photos">Upload reference image</label>
+	    <input id="photos" type="image" />
+
+	    <button onClick={handleSubmit}>Submit</button>
+	  </form>
+	  </div>
+
+	  {/*Right section*/}
+	  <div className="col-span-2">
+		<p className="font-medium text-2xl pb-5">Get Inpired</p>
+	  	<div className="grid grid-cols-2 my-5">
+		  <img src={refPhoto1} alt="Reference image 1" className="object-cover h-50 w-50" />
+	  	  <img src={refPhoto2} alt="Reference image 2" className="object-cover h-50 w-50" />
+		  <img src={refPhoto2} alt="Reference image 3" className="object-cover h-50 w-50" />
+	  	  <img src={refPhoto4} alt="Reference image 4" className="object-cover h-50 w-50" />
+	  </div>
+
+	  	<p className="text-xl">Custom order process:</p>
+	  	<ul>
+		  {process.map(p => (
+		    <li key=(item)>{p}</li>
+			  
+		  ))}
+	  </ul>
+	  </div>
+	  </div>
+	  </div>
   );
 
 }
