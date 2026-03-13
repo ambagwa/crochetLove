6889db81fbf70c8670a46fed80abc933c159c611
ambagwa <ambagwaeugene09@gmail.com>
2026-03-13 22:33:14 +0300

    Add ProtectedRoute.jsx

---
+++ client/src/utils/ProtectedRoute.jsx
@@ -0,0 +1,7 @@
+import { navigate } from 'react-router-dom';
+
+export const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+
+	return token ? children : <Navigate to="/login" replace />;
+};
