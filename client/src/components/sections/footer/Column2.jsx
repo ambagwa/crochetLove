ad9258b945d818529c30f3d766a77cc5acdb93d9
ambagwa <ambagwaeugene09@gmail.com>
2026-03-24 22:11:46 +0300

    Modify footer

--- client/src/components/sections/footer/Column2.jsx
+++ client/src/components/sections/footer/Column2.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Column2 = () => {
   return (
     <div className="py-2 text-2xl">
-            <a href="#" className="block my-2 text-2xl ">Home</a>
-            <a href="#" className="block my-2 text-2xl">Shop All Products</a>
-            <a href="#" className="block my-2 text-2xl">Custom orders</a>
+            <a href="#" className="block my-2 text-xl ">Home</a>
+            <a href="#" className="block my-2 text-xl">Shop All Products</a>
+            <a href="#" className="block my-2 text-xl">Custom orders</a>
         </div>
   )
 }
--- client/src/components/sections/footer/Column3.jsx
+++ client/src/components/sections/footer/Column3.jsx
@@ -1,20 +1,29 @@
 import { Button } from "@/components/ui/button";
+import { FaPhone } from "react-icons/fa";
+import { CgMailOpen } from "react-icons/cg";
+import { FaLocationDot } from "react-icons/fa6";
 
 const Column3 = () => {
   return (
     <div className="py-2 flex flex-col mx-auto">
-      <p className="text-2xl font-medium">Get in Touch</p>
+      <p className="text-xl font-medium">Get in Touch</p>
       <div className="flex my-3 text-center gap-2">
-        <Button size="icon">{/*PPhone icon*/}</Button>
-        <p className="text-xl">0713518279</p>
+        <div className="me-1 w-6 h-6">
+          <FaPhone className="w-5 h-5 mt-1" />
+        </div>
+        <p className="text-lg">0713518279</p>
       </div>
       <div className="flex my-3 text-center gap-2">
-        <Button size="icon">{/*Email icon*/}</Button>
-        <p className="text-xl">hello@crochetlove.com</p>
+        <div className="me-1 w-6 h-6">
+          <CgMailOpen className="w-5 h-5 mt-1" />
+        </div>
+        <p className="text-lg">hello@crochetlove.com</p>
       </div>
       <div className="flex my-3 text-center gap-2">
-        <Button size="icon">{/*Location icon*/}</Button>
-        <p className="text-xl">Nairobi, Kenya</p>
+        <div className="me-1 w-6 h-6">
+          <FaLocationDot className="w-5 h-5 mt-1" />
+        </div>
+        <p className="text-lg">Nairobi, Kenya</p>
       </div>
     </div>
   );
