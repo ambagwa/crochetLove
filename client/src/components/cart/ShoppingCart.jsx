f9c2f764a2ab212875ec7c7e6d87201a84e4f1b2
ambagwa <ambagwaeugene09@gmail.com>
2026-04-23 10:04:51 +0300

    Implement background scroll lock in ShoppingCart and enhance button functionality

--- client/src/components/cart/ShoppingCart.jsx
+++ client/src/components/cart/ShoppingCart.jsx
@@ -2,6 +2,7 @@ import { TbXboxX } from "react-icons/tb";
 import image from "../../assets/images/artist-face.jfif";
 import { Button } from "../ui/button";
 import { NavLink } from "react-router-dom";
+import { useEffect } from "react";
 
 export const ShoppingCart = ({ isOpen, onClose }) => {
   const products = [
@@ -9,8 +10,23 @@ export const ShoppingCart = ({ isOpen, onClose }) => {
     { id: 3, image: image, title: "Shack Dat", price: 988 },
     { id: 2, image: image, title: "Prime Bag", price: 286 },
   ];
+
+  // Lock background scroll when cart is open
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    //Clean up on unmount
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+
   return (
-    <div className="p-10 min-h-screen relative">
+    <>
       {/**Backdrop */}
       <div
         className={`fixed inset-0 bg-black/50 z-40 transition-opacity duration-300 ${
@@ -23,7 +39,7 @@ export const ShoppingCart = ({ isOpen, onClose }) => {
 
       {/**Sliding drawer */}
       <div
-        className={`fixed top-0 right-0 h-full w-full bg-white sm:w-[400px] z-50 shadow-xl flex flex-col transition-trnsform duration-300 ease-in-out ${
+        className={`fixed top-0 right-0 h-full w-full bg-white sm:w-[500px] z-50 shadow-xl flex flex-col transition-transform duration-300 ease-in-out ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
@@ -32,7 +48,7 @@ export const ShoppingCart = ({ isOpen, onClose }) => {
           <h1 className="font-medium text-2xl">Shopping Cart</h1>
 
           {/** Close button */}
-          <TbXboxX className="text-3xl hover:text-orange" />
+          <TbXboxX className="text-3xl hover:text-orange" onClick={onClose} />
         </div>
 
         {/**Products container */}
@@ -94,21 +110,25 @@ export const ShoppingCart = ({ isOpen, onClose }) => {
             <p className="text-2xl opacity-880">Subtotal</p>
             <p className="text-2xl font-medium">sh. 3000</p>
           </div>
-          <NavLink
-            variant="orange"
-            to="login"
-            className="w-full my-2 text-xl sm:text-3xl font-medium"
-          >
-            Proceed to Checkout
-          </NavLink>
-          <NavLink
-            variant="ghost"
-            className="w-full my-2 text-xl sm:text-3xl font-medium"
-          >
-            View cart
-          </NavLink>
+
+          {/** Buttons */}
+          <div className="flex flex-col text-center">
+            <NavLink
+              to="login"
+              onClick={onClose}
+              className="w-full rounded-lg bg-orange-300 p-2 text-lg sm:text-xl font-medium hover:text-white hover:bg-orange-400"
+            >
+              Proceed to Checkout
+            </NavLink>
+            <NavLink
+              onClick={onClose}
+              className="w-full my-2 text-lg rounded-lg sm:text-xl font-medium hover:bg-orange-100 p-2"
+            >
+              View cart
+            </NavLink>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
