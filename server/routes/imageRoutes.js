cc1a4950fdc3692b34276d17caf1599852da89bb
ambagwa <ambagwaeugene09@gmail.com>
2026-04-27 06:46:51 +0300

    Add getImageById route to imageRoutes for image retrieval by ID

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const upload = require("../middlewares/uploadMiddleware");
-const { uploadSingleImage } = require("../controllers/imageController");
+const {
+  uploadSingleImage,
+  getImageById,
+} = require("../controllers/imageController");
 
 router.post("/single", upload.single("image"), uploadSingleImage);
+router.get("/:id", getImageById);
 
 module.exports = router;
